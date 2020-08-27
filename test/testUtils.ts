529a650a1b6a124aeedf1ce893db2c803b3c58ce
Manassarn Manoonchai <manassarn@gmail.com>
2020-08-27 23:37:25 +0700

    :rotating_light: Add types to suppress errors

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
+import { ReactElement } from 'react'
 // import { ThemeProvider } from "my-ui-lib"
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
@@ -14,7 +15,7 @@ const Providers = ({ children }) => {
   // )
 }
 
-const customRender = (ui, options = {}) =>
+const customRender = (ui: ReactElement, options = {}): RenderResult =>
   render(ui, { wrapper: Providers, ...options })
 
 // re-export everything
