c41f0101e39dd32f0d9c4e9081356cf171120d4a
Manassarn Manoonchai <manassarn@gmail.com>
2020-09-02 23:11:36 +0700

    :sparkles: Add functions to stats context

--- test/contexts/Stats.test.tsx
+++ test/contexts/Stats.test.tsx
@@ -1,15 +1,36 @@
 import React from 'react'
 import { render } from '../testUtils'
-import { StatsContext } from '@/contexts/Stats'
+import { StatsContext, StatsProvider } from '@/contexts/Stats'
 
-const renderWithProvider = (ui, { providerProps, ...renderOptions }) => {
+const renderWithProvider = (
+  ui,
+  { providerProps = {}, ...renderOptions } = {}
+) => {
   return render(
-    <StatsContext.Provider {...providerProps}>{ui}</StatsContext.Provider>,
+    <StatsProvider {...providerProps}>{ui}</StatsProvider>,
     renderOptions
   )
 }
 
-test('Renders correctly', () => {
+test('renders without props', () => {
+  const { getByText } = renderWithProvider(
+    <StatsContext.Consumer>
+      {({ correct, wrong, total }) => (
+        <>
+          <span>correct: {correct}</span>
+          <span>wrong: {wrong}</span>
+          <span>total: {total}</span>
+        </>
+      )}
+    </StatsContext.Consumer>
+  )
+
+  expect(getByText(/^correct:/).textContent).toBe('correct: 0')
+  expect(getByText(/^wrong:/).textContent).toBe('wrong: 0')
+  expect(getByText(/^total:/).textContent).toBe('total: 0')
+})
+
+test('renders with props', () => {
   const providerProps = {
     value: {
       correct: 1,
@@ -34,4 +55,48 @@ test('Renders correctly', () => {
   expect(getByText(/^correct:/).textContent).toBe('correct: 1')
   expect(getByText(/^wrong:/).textContent).toBe('wrong: 2')
   expect(getByText(/^total:/).textContent).toBe('total: 3')
+})
+
+test('can increment count & reset', () => {
+  const providerProps = {
+    value: {
+      correct: 1,
+      wrong: 2,
+      total: 3,
+    },
+  }
+
+  const { getByText } = renderWithProvider(
+    <StatsContext.Consumer>
+      {({ correct, wrong, total, incrementCorrect, incrementWrong, reset }) => (
+        <>
+          <span>correct: {correct}</span>
+          <span>wrong: {wrong}</span>
+          <span>total: {total}</span>
+          <button onClick={incrementCorrect}>Increment Correct</button>
+          <button onClick={incrementWrong}>Increment Wrong</button>
+          <button onClick={reset}>Reset</button>
+        </>
+      )}
+    </StatsContext.Consumer>,
+    { providerProps }
+  )
+
+  expect(getByText(/^correct:/).textContent).toBe('correct: 1')
+  expect(getByText(/^total:/).textContent).toBe('total: 3')
+
+  getByText(/Increment Correct/).click()
+  expect(getByText(/^correct:/).textContent).toBe('correct: 2')
+  expect(getByText(/^wrong:/).textContent).toBe('wrong: 2')
+  expect(getByText(/^total:/).textContent).toBe('total: 4')
+
+  getByText(/Increment Wrong/).click()
+  expect(getByText(/^correct:/).textContent).toBe('correct: 2')
+  expect(getByText(/^wrong:/).textContent).toBe('wrong: 3')
+  expect(getByText(/^total:/).textContent).toBe('total: 5')
+
+  getByText(/Reset/).click()
+  expect(getByText(/^correct:/).textContent).toBe('correct: 0')
+  expect(getByText(/^wrong:/).textContent).toBe('wrong: 0')
+  expect(getByText(/^total:/).textContent).toBe('total: 0')
 })
